0c58cca0e3c0b711140eec7dca9369219f26f841
Saqlain <SaqlainSheikh0911@gmail.com>
2025-12-30 16:36:24 +0530

    Terraform codebase cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,10 @@ module "compute" {
 }
 
 resource "local_file" "ansible_inventory" {
-  content  = <<EOT
-  [webservers]
-  ${module.compute.instance_public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../id_rsa_project
+  content = <<-EOT
+    [webservers]
+    ${module.compute.instance_public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../id_rsa_project
   EOT
+
   filename = "../ansible/inventory/hosts.ini"
 }
